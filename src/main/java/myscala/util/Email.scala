d891ca5dd2eedd68becaaa91f0d84336f910f8e6
应卓 <yingzhor@gmail.com>
2018-04-09 12:45:50 +0800

    update

---
+++ src/main/java/myscala/util/Email.scala
@@ -0,0 +1,30 @@
+/*                              _
+  _ __ ___  _   _ ___  ___ __ _| | __ _
+ | '_ ` _ \| | | / __|/ __/ _` | |/ _` |
+ | | | | | | |_| \__ \ (_| (_| | | (_| |
+ |_| |_| |_|\__, |___/\___\__,_|_|\__,_|
+            |___/
+
+ https://github.com/yingzhuo/myscala
+*/
+package myscala.util
+
+
+object Email {
+
+    private val EmailRegex: String = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+
+    def unapplySeq[T](arg: T)(implicit transform: T => String): Option[(String, Seq[String])] = arg match {
+        case _ if isValid(arg) =>
+            val parts = arg.split("@")
+            Option((parts(0), parts(1).split(raw"\.")))
+        case _ => None
+    }
+
+    def isValid[T](s: T)(implicit transform: T => String): Boolean = s match {
+        case null => false
+        case _ if s.matches(Email.EmailRegex) => true
+        case _ => false
+    }
+
+}
